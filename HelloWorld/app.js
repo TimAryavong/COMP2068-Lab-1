848da31874e579dfda613e4f0ae84fe5ae3391f4
TimAryavong <200176280@student.georgianc.on.ca>
2020-05-29 18:17:46 -0400

    commit hello world

---
+++ HelloWorld/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var http = require('http'); //what is require???
+http.createServer(function (req, res) { //http server, what is req? What is res?
+    res.writeHead(200, { //why does res.writehead request
+        "Content-Type": "text/plain"
+    });
+    res.end('Hello World');
+}).listen(3000);
+console.log("Server listening on port 3000") //ctrl + f5 to run is faster
